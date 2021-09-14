0c1522ed669c44a33dad29315fb223fb1ba480df
Richie <rsaldiva@gmail.com>
2021-09-14 15:44:48 -0400

    add gitignore and .env file for api key

--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -2,7 +2,6 @@
 const baseURL = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&zip='
 
 
-
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
