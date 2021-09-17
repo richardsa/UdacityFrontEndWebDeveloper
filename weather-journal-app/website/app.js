0cc0313d2486ea77235d3effb15522c17b4dde2b
Richie <rsaldiva@gmail.com>
2021-09-17 13:53:47 -0400

    fix date function to return correct month

--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -8,7 +8,7 @@ const feelingsElement = document.getElementById('content');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate = d.getMonth()+1 + '.' + d.getDate() + '.' + d.getFullYear();
 
 
 // handle form submit
