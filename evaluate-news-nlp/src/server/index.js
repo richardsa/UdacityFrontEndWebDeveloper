3e634a3e954ba76a1fc1cd9c67b91c69c8e60a7f
Richie <rsaldiva@gmail.com>
2021-10-04 13:23:56 -0400

    update css to scss and load correctly on build

--- evaluate-news-nlp/src/server/index.js
+++ evaluate-news-nlp/src/server/index.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -16,6 +19,7 @@ app.get('/', function (req, res) {
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
+    console.log(`Your API key is ${process.env.API_KEY}`);
 })
 
 app.get('/test', function (req, res) {
