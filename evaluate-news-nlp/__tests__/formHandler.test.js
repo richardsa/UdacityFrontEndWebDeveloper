de1cd87f07bf628da668cc4c76623a6aad72419a
Richie <rsaldiva@gmail.com>
2021-10-13 13:24:47 -0400

    add jest tests

---
+++ evaluate-news-nlp/__tests__/formHandler.test.js
@@ -0,0 +1,7 @@
+import { handleSubmit } from "../src/client/js/formHandler";
+describe("Testing if defined", () => {
+  test("Testing handleSubmit() function exists", () => {
+    expect(handleSubmit).toBeDefined();
+
+  })
+});
